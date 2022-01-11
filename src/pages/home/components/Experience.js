f96713542741dd1c35b4869a05b3f06aea0ec6c4
Wayne Richen <wayne.richen@gmail.com>
2022-01-11 09:15:17 +0800

    Add API to dev skill

--- src/pages/home/components/Experience.js
+++ src/pages/home/components/Experience.js
@@ -6,22 +6,26 @@ const Experience = ({ state }) => {
         <span className="text-2xl sm:text-3xl text-tan font-bold">
           {state.experience.title}
         </span>
-        {state.experience.history.map((job) => (
-          <div className="my-2 w-full">
-            <span className="text-xl sm:text-2xl font-bold">{job.company}</span>
-            <br />
-            <span className="text-base sm:text-xl leading-none sm:leading-loose">
-              {job.jobtitle}
-            </span>
-            <div className="w-full">
-              <ul class="list-disc list-inside">
-                {job.jobs.map((job) => (
-                  <li>{job}</li>
-                ))}
-              </ul>
+        <div className="grid grid-cols-1 divide-y">
+          {state.experience.history.map((job) => (
+            <div className="py-2 w-full">
+              <span className="text-xl sm:text-2xl font-bold">
+                {job.company}
+              </span>
+              <br />
+              <span className="text-base sm:text-xl leading-none sm:leading-loose">
+                {job.jobtitle}
+              </span>
+              <div className="w-full">
+                <ul class="list-disc list-inside">
+                  {job.jobs.map((job) => (
+                    <li>{job}</li>
+                  ))}
+                </ul>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
